5674300ed76c8664ede7b11911417e46f2d87b80
Allan Medeiros Minato <allan.minato@gmail.com>
2023-01-04 18:58:04 -0300

    Resolução exercício 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Pokedex from './components/Pokedex';
 
 function App() {
   return (
-    <h1>Exercise - Pokedex</h1>
+
+    <div>
+      <h1>Exercise - Pokedex</h1>
+      <Pokedex />
+    </div>
   );
 }
 
---
+++ src/components/Pokedex.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Pokedex extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pokédex</h1>
+        <ul />
+      </div>
+    );
+  }
+}
